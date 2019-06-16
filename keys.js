114f3902c829e5123da8f0f33114d7233f259399
Michaela B <m.a.banczak@gmail.com>
2019-06-16 18:17:19 -0500

    added OMDB functionality

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
-console.log('this is loaded');
+// console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.bc7ded3ab8b84d7788c809fa725aee5f,
-  secret: process.env.1fe4fbef6c8948f18547bbb645444dd1,
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET,
 };
--- liri.js
+++ liri.js
@@ -1,18 +1,54 @@
 //calling necessary node packages and files
-require("dotenv").config();
+require("dotenv").config('./*.env');
+var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
 var axios = require('axios');
 var moment = require('moment');
-var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
 
-var songEntry = argv[2];
-var spotifyThisSong = function() {
-  spotify.search({ type: 'track', query: songEntry})
-  .then(function(response) {
-    console.log(response);
+var axios = require("axios");
+
+// Grab the movieName which will always be the third node argument.
+var movieName = process.argv[2];
+
+// Then run a request with axios to the OMDB API with the movie specified
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+// This line is just to help us debug against the actual URL.
+console.log(queryUrl);
+
+axios.get(queryUrl).then(
+  function(response) {
+    console.log("---------------------------------------");
+    console.log("Cast: " + response.data.Actors);
+    console.log("Release Year: " + response.data.Year);
+    console.log("Title: " + response.data.Title);
+    console.log("Cast: " + response.data.Actors);
+    console.log("IMDB Rating: " + response.data.imdbRating);
+    // console.log("Rotten Tomatoes Rating: " + response.data.Ratings[0][1].value);
+    console.log("Country: " + response.data.Country);
+    console.log("Language: " + response.data.Language);
+    console.log("Plot: " + response.data.Plot);
+    console.log("---------------------------------------");
   })
-  .catch(function(err) {
-    console.log(err);
+  .catch(function(error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log("---------------Data---------------");
+      console.log(error.response.data);
+      console.log("---------------Status---------------");
+      console.log(error.response.status);
+      console.log("---------------Status---------------");
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an object that comes back with details pertaining to the error that occurred.
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log("Error", error.message);
+    }
+    console.log(error.config);
   });
-};
+
