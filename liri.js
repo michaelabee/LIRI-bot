0353882e60e339a4b1e152f5b412390705cf41ab
Michaela B <m.a.banczak@gmail.com>
2019-06-16 20:39:28 -0500

    added search concert functionality

--- liri.js
+++ liri.js
@@ -65,5 +65,56 @@ var movieThis = function() {
 
 };
 
-movieThis();
+var concertThis = function() {
+  
+  // Store all of the arguments in an array
+    var nodeArgs = process.argv;
+  
+  // Create an empty variable for holding the movie name
+    var artist = "";
+  
+    for (var i = 2; i < nodeArgs.length; i++) {
+  
+      if (i > 2 && i < nodeArgs.length) {
+        artist = artist + "+" + nodeArgs[i];
+      } else {
+        artist += nodeArgs[i];
+  
+      }
+    }
+  
+  // Then run a request with axios to the Bands in Town API with the movie specified
+    var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+  
+    axios.get(queryUrl).then(
+      function(response) {
+        console.log("---------------------------------------");
+        console.log(response.data[0].venue.city);
+        console.log(response.data[0].venue.name);
+        console.log(moment(response.data[0].datetime).format("MM/DD/YYYY"));
+        console.log("---------------------------------------");
+      })
+      .catch(function(error) {
+        if (error.response) {
+          
+          console.log("---------------Data---------------");
+          console.log(error.response.data);
+          console.log("---------------Status---------------");
+          console.log(error.response.status);
+          console.log("---------------Status---------------");
+          console.log(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an object that comes back with details pertaining to the error that occurred.
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log("Error", error.message);
+        }
+        console.log(error.config);
+      });
+  
+  };
+  
+concertThis();
 
